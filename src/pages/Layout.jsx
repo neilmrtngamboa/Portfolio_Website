f414cc3cfa867f2c8f704c396c5f9903698b4f09
neilmrtngamboa <nmg.gamboa@gmail.com>
2024-05-08 15:15:30 +0800

    added styling to the navbar links

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,12 +1,14 @@
 import { Outlet, Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import './Styling/Layout.css'
+
 function Layout() {
 
     return (
       <main className="d-flex flex-column min-vh-100">
         
-        <nav className="navbar navbar-expand-lg bg-dark p-3">
+        <nav className="navbar navbar-expand-lg bg-dark shadow p-3">
             <div className="container-fluid">
                 <Link className="navbar-brand text-white" to='/'>Neil Martin Gamboa
                     <div><h5 className="fs-6 text-secondary fw-light">Front End Developer</h5></div>
@@ -16,8 +18,8 @@ function Layout() {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div className="navbar-nav mx-auto">
-                        <Link className="nav-link text-light" to='about'>About</Link>
-                        <Link className="nav-link text-light" to='contact'>Contact</Link>
+                        <Link className="nav-link links" to='about'>About</Link>
+                        <Link className="nav-link links" to='contact'>Contact</Link>
                     </div>
                 </div>
             </div>
@@ -30,8 +32,8 @@ function Layout() {
         <footer className="bg-dark text-center text-light p-3 mt-auto">
                 <h5 className="fw-bold">Neil Martin Gamboa</h5>
                 <div className="fs-3">
-                    <a href="https://github.com/neilmrtngamboa" target="blank" className="text-secondary"><FontAwesomeIcon icon={faGithub} /></a>
-                    <a href="https://www.linkedin.com/in/neilmartingamboa/" target="blank" className="text-secondary"><FontAwesomeIcon icon={faLinkedin} className="ms-3" /></a>
+                    <a href="https://github.com/neilmrtngamboa" target="blank" className="icons"><FontAwesomeIcon icon={faGithub} /></a>
+                    <a href="https://www.linkedin.com/in/neilmartingamboa/" target="blank" className="icons"><FontAwesomeIcon icon={faLinkedin} className="ms-3" /></a>
                     
                 </div>
                 
