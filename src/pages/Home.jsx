53d83c4a6a73c94f51a2026d4a5761aefaa43a58
neilmrtngamboa <nmg.gamboa@gmail.com>
2025-01-15 21:11:09 +0800

    Changed the hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,29 +19,51 @@ function Home() {
 
 //Main
   return (
-    <main className='mt-3 p-3'>
+    <main id='bg' className='p-5'>
+
       <div className="row">
 
         <div className="col-md-6 my-auto" data-aos='fade-up' data-aos-duration='3000'>
 
-          <h3 className='text-light'>Hi! 👋🏼 I'm</h3>
-          <h1 className='display-1 fw-bold text-light'>Neil Martin Gamboa</h1>
-          <h6 className='fs-5 text-secondary fw-light'>Front End Developer based in <b>Malabon City, NCR</b> 📍</h6>
-          <button className="mt-3 bn5">
-            <a href={resume} download='resume' id='CV'>Download CV</a>
-          </button>
+          <h3 className='text-light fw-lighter'>Hi there! I'm</h3>
+          <h1 className='display-1 fw-bold text-light'>Neil Martin Gamboa,</h1>
+          <h6 className='display-5 fw-bold text-light'>Front-End Developer</h6>
+          <p className='fs-4 text-light fw-lighter'>Bringing ideas to life with code.</p>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-5">
+                <button className="mt-3" id='dlCV'>
+                <a href={resume} download='resume' id='CV'>Download CV</a>
+                </button>
+              </div>
+
+              <div className="col-md-5">
+                <button className="mt-3" id='dlCV'>
+                  Learn More
+                </button>
+              </div>
+            
+
+            </div>
+            
+          </div>
+          
         </div>
 
         <div className="col-md-6" data-aos='zoom-in' data-aos-duration='3000'>
 
           <div className="container">
-            <img src={img} alt="mainImg" className='img-fluid ms-1'
+            <img src={img} alt="mainImg" className='img-fluid ms-1' id='mainImg'
             />
           </div>
 
         </div>
 
       </div>
+
+      <section className='bg-black mt-5 p-5'>
+        <h2 className='fs-1 text-light text-center'>Who is Neil?</h2>
+      </section>
 
       <section className='mt-5 p-5 bg-black rounded shadow'>
         <h1 className='display-1 text-light fw-bold'>Recent Projects</h1>
