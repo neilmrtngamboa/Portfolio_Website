2ac066444803d3ad393f31125e26c8263146e464
neilmrtngamboa <nmg.gamboa@gmail.com>
2025-01-19 22:35:00 +0800

    Linked the 'View All' button to the AllProjects Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -217,7 +217,7 @@ function Home() {
 
               </div>
               <span className='mt-3'>
-                <Link to='' id='viewProj' style={{ fontFamily: 'Courier' }}>
+                <Link to='allprojects' id='viewProj' style={{ fontFamily: 'Courier' }}>
                   View all
                   <svg xmlns="http://www.w3.org/2000/svg" className='ms-2 mx-auto my-auto' width="16" height="16" fill="currentColor" class="bi bi-arrow-return-right" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M1.5 1.5A.5.5 0 0 0 1 2v4.8a2.5 2.5 0 0 0 2.5 2.5h9.793l-3.347 3.346a.5.5 0 0 0 .708.708l4.2-4.2a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 8.3H3.5A1.5 1.5 0 0 1 2 6.8V2a.5.5 0 0 0-.5-.5" />
