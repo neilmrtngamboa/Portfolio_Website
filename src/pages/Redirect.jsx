e58c29d11d984e00570f9be1082df0818aff34c3
neilmrtngamboa <nmg.gamboa@gmail.com>
2025-01-19 23:38:30 +0800

    Made changes to the 404 Redirect page

--- src/pages/Redirect.jsx
+++ src/pages/Redirect.jsx
@@ -1,10 +1,20 @@
-
+import './Styling/Redirect.css'
+import { Link } from 'react-router-dom'
 function NotFound() {
 
     return (
-      <>
-        <h1 className="text-center fs-1">404 NOT FOUND!</h1>
-      </>
+      <main className="bg-white min-vh-100 text-center">
+        <h1 className="fw-bolder" id='oops'>404</h1>
+        <div className='fs-2 fw-lighter not' id='not'>that page cannot be found</div>
+        <div className='fs-2 fw-lighter not' id='not'>let's get you back</div>
+        <Link to='/'>
+          <button id='backBtn'>
+            Back to Homepage
+          </button>
+        
+        </Link>
+
+      </main>
     )
   }
   
